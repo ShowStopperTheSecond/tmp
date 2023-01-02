9f161bedb9044b5eeba2eae71386e01c2a833ef9
Javid <62745203+TheKiteFlier@users.noreply.github.com>
2023-01-02 16:29:31 +0330

    Update __init__.py
    sdf

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -12,37 +12,37 @@ from .sar import *
 
 # try to instanciate datasets
 import sys
-try:
-    web_images = RandomWebImages(0, 52)
-    print("web_images: successful ")
-except AssertionError as e:
-    print(f"Dataset web_images not available, reason: {e}", file=sys.stderr)
+# try:
+#     web_images = RandomWebImages(0, 52)
+#     print("web_images: successful ")
+# except AssertionError as e:
+#     print(f"Dataset web_images not available, reason: {e}", file=sys.stderr)
 
-try:
-    aachen_db_images = AachenImages_DB()
-    print("aachen_db_images: successful ")
+# try:
+#     aachen_db_images = AachenImages_DB()
+#     print("aachen_db_images: successful ")
 
-except AssertionError as e:
-    print(f"Dataset aachen_db_images not available, reason: {e}", file=sys.stderr)
+# except AssertionError as e:
+#     print(f"Dataset aachen_db_images not available, reason: {e}", file=sys.stderr)
 
-try:
-    aachen_style_transfer_pairs = AachenPairs_StyleTransferDayNight()
-    print("aachen_style_transfer_pairs: successful ")
+# try:
+#     aachen_style_transfer_pairs = AachenPairs_StyleTransferDayNight()
+#     print("aachen_style_transfer_pairs: successful ")
 
-except AssertionError as e:
-    print(f"Dataset aachen_style_transfer_pairs not available, reason: {e}", file=sys.stderr)
+# except AssertionError as e:
+#     print(f"Dataset aachen_style_transfer_pairs not available, reason: {e}", file=sys.stderr)
 
-try:
-    aachen_flow_pairs = AachenPairs_OpticalFlow()
-    print("aachen_flow_pairs : successful ")
-except AssertionError as e:
-    print(f"Dataset aachen_flow_pairs not available, reason: {e}", file=sys.stderr)
+# try:
+#     aachen_flow_pairs = AachenPairs_OpticalFlow()
+#     print("aachen_flow_pairs : successful ")
+# except AssertionError as e:
+#     print(f"Dataset aachen_flow_pairs not available, reason: {e}", file=sys.stderr)
 
-try:
-    sar_db_images = SARImages_DB()
-    print("sar_db_images: successful ")
-except AssertionError as e:
-    print(f"Dataset SAR_db_images not available, reason: {e}", file=sys.stderr)
+# try:
+#     sar_db_images = SARImages_DB()
+#     print("sar_db_images: successful ")
+# except AssertionError as e:
+#     print(f"Dataset SAR_db_images not available, reason: {e}", file=sys.stderr)
 
 
 try:
