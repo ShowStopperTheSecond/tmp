f37bc409601165540d048a1185076531c39cd750
javid <norouzi.javid@protonmail.com>
2023-08-12 01:37:17 +0330

    latest update

--- nets/reliability_loss.py
+++ nets/reliability_loss.py
@@ -175,7 +175,9 @@ class CustomReliabilityLoss (CustomPixelAPLoss):
 
 
     def loss_from_ap(self, ap, rel):
-        return 1 - self.cosine_similarity(ap, rel) - (1-rel)*self.base
+        return torch.abs(1 - self.cosine_similarity(ap, rel) - (1-rel)*self.base)
+        # return 1 - self.cosine_similarity(ap, rel) - (1-rel)*self.base
+
         # return 1 - ap*rel - (1-rel)*self.base
 
 
