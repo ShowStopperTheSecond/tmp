85f99166984e3b9e6c9ffbfc2e138d0f0b9dcdef
javid <norouzi.javid@protonmail.com>
2023-08-11 04:49:59 +0330

    latest update

--- nets/reliability_loss.py
+++ nets/reliability_loss.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from nets.ap_loss import APLoss
+from nets.pnp_loss import  PNPLoss
 
 
 class PixelAPLoss (nn.Module):
@@ -42,6 +43,8 @@ class PixelAPLoss (nn.Module):
         return loss
 
 
+
+
 class ReliabilityLoss (PixelAPLoss):
     """ same than PixelAPLoss, but also train a pixel-wise confidence
         that this pixel is going to have a good AP.
@@ -54,6 +57,60 @@ class ReliabilityLoss (PixelAPLoss):
 
     def loss_from_ap(self, ap, rel):
         return 1 - ap*rel - (1-rel)*self.base
+
+
+class PixelPNPLoss(nn.Module):
+    """ Computes the pixel-wise AP loss:
+        Given two images and ground-truth optical flow, computes the AP per pixel.
+
+        feat1:  (B, C, H, W)   pixel-wise features extracted from img1
+        feat2:  (B, C, H, W)   pixel-wise features extracted from img2
+        aflow:  (B, 2, H, W)   absolute flow: aflow[...,y1,x1] = x2,y2
+    """
+
+    def __init__(self, sampler,   b=2, alpha=1, anneal=0.01, variant="Dq",):
+        nn.Module.__init__(self)
+        self.b = b
+        self.alpha = alpha
+        self.anneal = anneal
+        self.variant = variant
+        self.pnploss = PNPLoss(b, alpha,anneal)
+        self.name = 'pixPNP'
+        self.sampler = sampler
+
+
+    def forward(self, descriptors, aflow, **kw):
+        # subsample things
+        scores, gt, msk, qconf = self.sampler(descriptors, kw.get('reliability'), aflow)
+
+        # compute pixel-wise PNP Loss
+        n = qconf.numel()
+        if n == 0: return 0
+
+        pnp = self.pnploss(scores, gt)['loss']['losses']
+        pnp =  pnp.view(msk.shape)
+
+
+        pixel_loss = self.loss_from_pnp(pnp, qconf)
+
+        loss = pixel_loss[msk].mean()
+
+        return loss
+
+
+class ReliabilityPNPLoss(PixelPNPLoss):
+    """ same than PixelAPLoss, but also train a pixel-wise confidence
+        that this pixel is going to have a good AP.
+    """
+
+    def __init__(self, sampler, base=0.5, **kw):
+        PixelPNPLoss.__init__(self, sampler, **kw)
+        assert 0 <= base < 1
+        self.base = base
+        self.name = 'reliability'
+
+    def loss_from_pnp(self, pnp, rel):
+        return 1 - pnp * rel - (1 - rel) * self.base
 
 
 
