a287894d1f212edf52427f06d2eb062ab4845ce1
javid <norouzi.javid@protonmail.com>
2023-08-11 23:49:11 +0330

    latest update

--- nets/reliability_loss.py
+++ nets/reliability_loss.py
@@ -10,6 +10,8 @@ from nets.ap_loss import APLoss
 from nets.pnp_loss import  PNPLoss
 
 
+
+
 class PixelAPLoss (nn.Module):
     """ Computes the pixel-wise AP loss:
         Given two images and ground-truth optical flow, computes the AP per pixel.
@@ -51,14 +53,16 @@ class ReliabilityLoss (PixelAPLoss):
     """
     def __init__(self, sampler, base=0.5, **kw):
         PixelAPLoss.__init__(self, sampler, **kw)
-        # assert 0 <= base < 1
+        assert 0 <= base < 1
         self.base = base
         self.name = 'reliability'
-        self.cosine_similarity =  nn.CosineSimilarity(dim=1, eps=1e-6)
+
 
     def loss_from_ap(self, ap, rel):
-        return ap  + (1 - self.cosine_similarity(ap, rel))
-        # return 1 - ap*rel - (1-rel)*self.base
+        return 1 - ap*rel - (1-rel)*self.base
+
+
+
 
 
 
@@ -117,4 +121,56 @@ class ReliabilityPNPLoss(PixelPNPLoss):
         return 1 - pnp * rel - (1 - rel) * self.base
 
 
+
+
+
+class CustomPixelAPLoss (nn.Module):
+    """ Computes the pixel-wise AP loss:
+        Given two images and ground-truth optical flow, computes the AP per pixel.
+        
+        feat1:  (B, C, H, W)   pixel-wise features extracted from img1
+        feat2:  (B, C, H, W)   pixel-wise features extracted from img2
+        aflow:  (B, 2, H, W)   absolute flow: aflow[...,y1,x1] = x2,y2
+    """
+    def __init__(self, sampler, nq=20):
+        nn.Module.__init__(self)
+        self.aploss = APLoss(nq, min=0, max=1, euc=False)
+        self.name = 'pixAP'
+        self.sampler = sampler
+
+    def loss_from_ap(self, ap, rel):
+        return 1 - ap
+
+    def forward(self, descriptors, aflow, **kw):
+        # subsample things
+        scores, gt, msk, qconf = self.sampler(descriptors, kw.get('reliability'), aflow)
+        
+        # compute pixel-wise AP
+        n = qconf.numel()
+        if n == 0: return 0
+        scores, gt = scores.view(n,-1), gt.view(n,-1)
+        ap = self.aploss(scores, gt).view(msk.shape)
+
+        pixel_loss = self.loss_from_ap(ap, qconf)
+        
+        loss = pixel_loss[msk].mean()
+        return loss
+
+
+
+
+class CustomReliabilityLoss (CustomPixelAPLoss):
+    """ same than PixelAPLoss, but also train a pixel-wise confidence
+        that this pixel is going to have a good AP.
+    """
+    def __init__(self, sampler, base=0.5, **kw):
+        PixelAPLoss.__init__(self, sampler, **kw)
+        assert 0 <= base < 1
+        self.base = base
+        self.name = 'reliability'
+        self.cosine_similarity =  nn.CosineSimilarity(dim=1, eps=1e-6)
+
+    def loss_from_ap(self, ap, rel):
+        return ap  + (1 - self.cosine_similarity(ap, rel))
+        # return 1 - ap*rel - (1-rel)*self.base
 
