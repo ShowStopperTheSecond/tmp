fd4ff0e085431016034d273cac73608c809895bf
javid <norouzi.javid@protonmail.com>
2023-08-19 02:37:37 +0330

    latest updates

--- nets/patchnet.py
+++ nets/patchnet.py
@@ -1660,13 +1660,13 @@ class Custom_18_Fast_Quad_L2Net_Selu (PatchNet):
     def __init__(self, dim=128, mchan=4, relu22=False, downsample_factor=2, **kw ):
 
         PatchNet.__init__(self, **kw)
-        self.bn = True
+        self.bn = False
         self.downsample_factor = downsample_factor
+        self._add_conv(  4*mchan,relu=False, gcu=False, selu=True)
         self._add_conv(  8*mchan,relu=False, gcu=False, selu=True)
-        self._add_conv(  8*mchan,relu=False, gcu=False, selu=True)
-        self._add_conv( 16*mchan, k_pool = downsample_factor,relu=False, gcu=False, selu=True,pool_type='avg') # added avg pooling to decrease img resolution
+        self._add_conv( 12*mchan, k_pool = downsample_factor,relu=False, gcu=False, selu=True,pool_type='avg') # added avg pooling to decrease img resolution
         self._add_conv( 16*mchan,relu=False, gcu=False, selu=True)
-        self._add_conv( 32*mchan,relu=False, gcu=False, stride=2, selu=True)
+        self._add_conv( 24*mchan,relu=False, gcu=False, stride=2, selu=True)
         # self._add_conv( 32*mchan,relu=False, gcu=False, selu=True)
         
         # replace last 8x8 convolution with 3 2x2 convolutions
